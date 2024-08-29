641901e5b704ee6d277c8e7c2f6e250ab4602327
Sotatek-NhiNguyen2 <174430306+Sotatek-NhiNguyen2@users.noreply.github.com>
2024-08-29 10:24:53 +0700

    feat: add base

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import ListNovel from "./pages/ListNovel";
+import ListFilm from "./pages/ListFilm";
+import ReviewNovel from "./pages/ReviewNovel";
+import ReviewFilm from "./pages/ReviewFilm";
+import Ancient from "./pages/Ancient";
+import Modern from "./pages/Modern";
 
 function App() {
   return (
@@ -7,8 +13,13 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/list-truyen" element={<ListNovel />} />
+        <Route path="/list-phim" element={<ListFilm />} />
+        <Route path="/review-truyen" element={<ReviewNovel />} />
+        <Route path="/review-phim" element={<ReviewFilm />} />
+        <Route path="/list-co-dai" element={<Ancient />} />
+        <Route path="/list-hien-dai" element={<Modern />} />
       </Routes>
-      <div>dfiobjfiovin</div>
     </>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,24 @@
+import CardFlim from "../component/CardFlim";
+import CardNovel from "../component/CardNovel";
 import Header from "../component/Header";
+import Introduction from "../component/Introduction";
 import SideBar from "../component/SideBar";
 import "../Css/home.css";
 
 function Home() {
   return (
     <>
-      <div className="body-content h-fit">
+      <div className="body-content h-full">
         <Header />
-        <div className="ms-auto flex">
-          <div className=""></div>
+        <div className="flex justify-center gap-28">
+          <div className="flex flex-col gap-[121px] items-center">
+            <Introduction />
+            <CardFlim />
+            <CardNovel />
+          </div>
           <SideBar />
         </div>
+        <div className="h-[300px] mt-[60px]"></div>
       </div>
     </>
   );
